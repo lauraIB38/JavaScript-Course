5b13eaf6d3ff0ee4a764a7f10272b4698eabad8d
Ibañez Blanco Laura <lauraibanezblanco@gmail.com>
2022-09-07 22:24:41 +0200

    funciones en js

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # JavaScript-Course
+## Tipos de Datos
+    const cadena = "5";
+    const numero = 5;
+    const condicion = false;
+    const nulo = null;
+    let indefinido;
+### Conversión implícita
+    Reglas: String es el tipo más fuerte así que todo lo que concatenemos con string pasará a string (mientras sea suma)
+                console.log(cadena + numero) = 55
+                console.log(numero - cadena) = 0
+            Number es el segundo tipo más fuerte de manera que  
+                console.log(numero + true) = 6
+                console.log(numero + nulo) = 5
+
+### Conversión explícita
+    String ()
+    Number ()
+    Boolean ()
+    ParseInt ()
+    ParseFloat ()
+
+        console.log(Number (numero)) = 5
+        console.log(Number (cadena)) = NaN
+        console.log(Boolean (cadena)) = True // Es true mientras tenga algún caracter y false si está vacío si es cero o si son null o indefinido
+        console.log(Boolean (cadena)) = NaN
--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log('hola')
+// funcion nominal. Se puede llamar a esta función antes de ser declarada
+function decirHola (nombre){
+    // ambito
+    return `Hola ${nombre}, bienvenida`;
+}
+const resultado1 = decirHola("Laura");
+console.log("despues de llamar", resultado1);
+
+// funcion anónima. Se almacenan en una vbl y el nombre de la función ser´ña el nombre de la vbl
+const obtenerSaludo = function (nombre){
+    return `Hola ${nombre}, bienvenido`;
+}
+const resultado2 = obtenerSaludo("Miguel");
+console.log("despues de llamar", resultado2)
+
+// la funciones nominales y las anónimas no son del todo equivalentes
+// a las anominas de le aplican las reglas de las constantes y por tanto no se puede ejecutar antes de declararlo
+const imprimirSaludo = function (mensaje) {
+    console.log(mensaje);
+}
+
+const alertSaludo = function (mensaje) {
+    alert(mensaje);
+}
+const saludar = function (obtener, imprimir) {
+    const saludo = obtener("Pablo");
+    imprimir(saludo);
+}
+
+// una función puede llamar a una función o callback
+saludar (obtenerSaludo, imprimirSaludo);
+
+// Funciones flecha
+//1. Son equivalentes que si hubieramos hecho lo mismo con funciones anónimas
+//2. Se pueden quitar los paréntesis cuendo solo tengan un argumento de entrada
+//3. Cuando sea una funcion que retorne directamente podemos eliminar las llaves y podemos hacer un return implicito
+/*
+const sumar = (a, b) => {
+   return a + b
+}
+*/
+const sumar = (a, b) => a + b
+const multiplicarPorDos = numero => numero * 2
+
+const suma = sumar(2, 3)
+const multiplicado = multiplicarPorDos(suma)
+console.log(multiplicado)
